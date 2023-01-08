3441dd7b6988f882310f083afd859618a303351c
Brian Henning <henninb@msn.com>
2023-01-08 07:32:26 -0600

    wip

---
+++ public/tool5/script.js
@@ -0,0 +1,20 @@
+function getGames() {
+  fetch('../api/cat-facts')
+  .then((res) => res.json())
+  .then((data) => {
+    let output = '<h2 class="mb-4">Cat Facts</h2>';
+    //data.forEach(function(fact){
+      output += `
+        <div class="card card-body mb-3">
+          <h3>${data.fact}</h3>
+        </div>
+      `;
+    //});
+    document.getElementById('output').innerHTML = output;
+  })
+}
+
+window.onload = function() {
+  console.log("onload called");
+  document.getElementById('getGames').addEventListener('click', getGames);
+}
