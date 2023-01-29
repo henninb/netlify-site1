db65df67d06c06622377b2039f2d39616048a58a
Brian Henning <henninb@msn.com>
2023-01-29 08:12:42 -0600

    wip

--- server.js
+++ server.js
@@ -23,6 +23,12 @@ app.get('/api/cat-facts', async (_req, response) => {
   response.send(JSON.stringify(data))
 });
 
+app.get('/api/board', async (_req, response) => {
+  res = await fetch('https://www.boredapi.com/api/activity')
+  data = await res.json()
+  response.send(JSON.stringify(data))
+});
+
 app.get('/api/baseball', async (_req, response) => {
   const year =  new Date().getFullYear()
   const url = new URL('https://statsapi.mlb.com/api/v1/schedule')
